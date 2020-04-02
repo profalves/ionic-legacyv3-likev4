3a3fed6c338024639e2fd52b19a72d53f9fc6f32
Rodrigo Alves <igo.sud@hotmail.com>
2020-04-01 21:36:26 -0300

    it begins

---
+++ README.md
@@ -0,0 +1,5 @@
+# Routing ionic v3 in v4
+
+This is a exemple of using the routing navigation like v3 in v4
+
+Follow this [Tutorial](https://www.joshmorony.com/converting-ionic-3-push-pop-navigation-to-angular-routing-in-ionic-4/)
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
 ];
 
 @NgModule({
