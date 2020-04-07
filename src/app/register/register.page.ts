fdebc2781029a789c64d3afc34ad40a850b583df
Rodrigo Alves <igo.sud@hotmail.com>
2020-04-07 12:21:27 -0300

    add modal and dialogs responses

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { AlertController, LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -21,9 +22,11 @@ export class RegisterPage implements OnInit {
     }
   };
 
-  constructor(private http: HttpClient) {
-
-  }
+  constructor(
+    public alertController: AlertController,
+    public loadingCtrl: LoadingController,
+    public http: HttpClient
+  ) { }
 
   ngOnInit() { }
 
@@ -37,5 +40,73 @@ export class RegisterPage implements OnInit {
     this.http.get(`${this.url}/products`).subscribe((response) => {
       console.log(response);
     });
+  }
+
+  async dialogAdd() {
+    const alert = await this.alertController.create({
+      header: 'Alerta',
+      subHeader: 'Adicionar funcionário',
+      message: 'Insira os dados do novo usuario.',
+      inputs: [{
+        name: "nome",
+        placeholder: "Nome"
+      }, {
+        name: "email",
+        placeholder: "E-mail"
+      }, {
+        name: "telefone",
+        placeholder: "Telefone"
+      }],
+      buttons: [{
+        text: "Cancelar"
+      }, {
+        text: "Salvar",
+        handler: async data => {
+          const loading = await this.loadingCtrl.create({
+            message: 'Carregando...'
+          });
+          await loading.present();
+
+          this.http.post(`${this.url}/test`, {
+            nome: data.nome,
+            email: data.email,
+            telefone: data.telefone
+          }, this.header)
+            .subscribe(async (resposta) => {
+              console.log(resposta);
+              await loading.dismiss();
+              this.requisicaoSucesso();
+            }, async (error) => {
+              console.log(error);
+              await loading.dismiss();
+              this.requisicaoErro(error);
+            });
+        }
+      }]
+    });
+
+    await alert.present();
+  }
+
+  async requisicaoSucesso() {
+    const alert = await this.alertController.create({
+      header: 'Sucesso',
+      subHeader: '',
+      message: 'O funcionário foi adicionado ao Banco de Dados',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  async requisicaoErro(error) {
+    const alert = await this.alertController.create({
+      header: 'Error',
+      subHeader: '',
+      message: error.message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 }
